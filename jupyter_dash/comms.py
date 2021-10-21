76fef4b47fcf6d54ab024267afa63d150f69bf27
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-10-21 14:32:54 -0700

    Rework how base_url_request is done to use AJAX instead of kernel comms.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- jupyter_dash/comms.py
+++ jupyter_dash/comms.py
@@ -1,53 +1,144 @@
 from ipykernel.comm import Comm
 import asyncio
+import http.server
+import socketserver
+import json
+from json import JSONDecodeError
+from http import HTTPStatus
+import time
+import logging
 
 __jupyter_config = {}
-__jupyter_config_future = asyncio.Future()
 __dash_comm = Comm(target_name="jupyter_dash")
 
 
-@__dash_comm.on_msg
-def _receive_message(msg):
-    msg_data = msg.get("content").get("data")
-    msg_type = msg_data.get("type", None)
-    if msg_type == "base_url_response":
-        __jupyter_config_future.set_result(msg_data)
+MAX_LENGTH = 512
 
 
-async def _get_jupyter_config():
-    __dash_comm.send({"type": "base_url_request"})
-
+def _set_jupyter_config(config):
     global __jupyter_config
-    __jupyter_config = await __jupyter_config_future
+    __jupyter_config = config
 
 
-# This task will be complete once __jupyter_config is populated.
-__jupyter_config_task = asyncio.create_task(_get_jupyter_config())
+class OneRequestServer(http.server.BaseHTTPRequestHandler):
+    """This is single purpose HTTP server thats sole purpose is to receive one AJAX.
+
+    The one AJAX request should be a JSON message from the JupyterDash browser plugin
+    in response to the Comm message "base_url_request_ajax".
+
+    """
+
+    error_content_type = "application/json"
+
+    def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger(__name__)
+        super().__init__(*args, **kwargs)
+
+    def send_bad_request(self, body):
+        self.send_response(HTTPStatus.BAD_REQUEST)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(body).encode())
+
+    def do_POST(self):
+        if self.headers["Content-Type"] != "application/json":
+            self.send_bad_request(
+                {
+                    "error": (
+                        'Content-Type must be "application/json", was "{}"'
+                    ).format(self.headers["Content-Type"])
+                }
+            )
+            return
+
+        content_length = int(self.headers["Content-Length"])
+        if content_length > MAX_LENGTH:
+            self.send_bad_request(
+                {
+                    "error": ("Content-Length must be less than {}, got {}").format(
+                        self.headers["Content-Length"], MAX_LENGTH
+                    )
+                }
+            )
+            return
+
+        post_data = self.rfile.read(content_length)
+
+        try:
+            data = post_data.decode()
+        except UnicodeError:
+            self.send_bad_request({"error": "Content must be valid UTF-8"})
+            return
+
+        try:
+            msg = json.loads(data)
+        except JSONDecodeError:
+            self.send_bad_request({"error": "Content must be valid JSON"})
+            return
+
+        for field in ["type", "server_url", "base_subpath", "frontend"]:
+            if field not in msg:
+                self.send_bad_request({"error": "Key {} was not found".format(field)})
+                return
+
+        _set_jupyter_config(msg)
+
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        response = {}
+        self.wfile.write(json.dumps(response).encode())
+
+        self.server.serving = False
+
+    def handle_timeout(self):
+        pass
+
+    def log_message(self, format, *args, **kwargs):
+        self.logger.info(
+            "%s - - [%s] %s\n"
+            % (self.address_string(), self.log_date_time_string(), format % args)
+        )
 
 
 async def _request_jupyter_config(timeout=1):
     """Attempt to complete task to retrieve jupyter config.
 
-    This coroutine will timeout after arg timeout seconds.
-    This coroutine can be awaited multiple times in case time is too short.
-    This coroutine is a no-op outside of Jupyter.
-
     """
 
-    # Heavily inspired by implementation of CaptureExecution in the
-    if __dash_comm.kernel is None:
-        # Not in jupyter setting
-        return
+    # Re-open the comm in case the frontend connection was lost (e.g. tab closed or
+    # refreshed).
+    __dash_comm.open()
 
-    try:
-        await asyncio.wait_for(asyncio.shield(__jupyter_config_task), timeout=timeout)
-    except asyncio.TimeoutError:
-        raise RuntimeError(
-            (
-                'Jupyter config not ready, re-run "await '
-                'JupyterDash.infer_jupyter_proxy_config()"'
+    # Start a server to receive the response from the browser plugin.
+    with socketserver.TCPServer(("", 0), OneRequestServer) as server:
+        server.timeout = 0.100
+        port = server.server_address[1]
+
+        # Now that socket is open, ask for AJAX request
+        __dash_comm.send({"type": "base_url_request_ajax", "port": str(port)})
+
+        if timeout is not None:
+            timeout_time = time.time() + timeout
+
+        server.serving = True
+
+        while server.serving:
+            server.handle_request()
+
+            if timeout is not None:
+                if time.time() > timeout_time:
+                    break
+
+        if server.serving:
+            # If the server is still serving, never got the reply!
+            raise RuntimeError(
+                (
+                    'Jupyter config not ready, re-run "await '
+                    'JupyterDash.infer_jupyter_proxy_config()"'
+                )
             )
-        )
 
 
 def get_jupyter_config():
@@ -56,7 +147,7 @@ def get_jupyter_config():
         # Not in jupyter setting
         return {}
 
-    if not __jupyter_config_future.done():
+    if not __jupyter_config:
         raise RuntimeError(
             (
                 'Jupyter config not ready, re-run "await '
