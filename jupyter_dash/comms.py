518f81716f81f1d7458a2395e23faacbdaee16a7
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-10-20 17:05:33 -0700

    Fix bug when JupyterLab is refreshed.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- jupyter_dash/comms.py
+++ jupyter_dash/comms.py
@@ -3,19 +3,19 @@ import asyncio
 
 __jupyter_config = {}
 __jupyter_config_future = asyncio.Future()
-__dash_comm = Comm(target_name='jupyter_dash')
+__dash_comm = Comm(target_name="jupyter_dash")
+
 
 @__dash_comm.on_msg
 def _receive_message(msg):
-    msg_data = msg.get('content').get('data')
-    msg_type = msg_data.get('type', None)
-    if msg_type == 'base_url_response':
+    msg_data = msg.get("content").get("data")
+    msg_type = msg_data.get("type", None)
+    if msg_type == "base_url_response":
         __jupyter_config_future.set_result(msg_data)
 
+
 async def _get_jupyter_config():
-    __dash_comm.send({
-                'type': 'base_url_request'
-            })
+    __dash_comm.send({"type": "base_url_request"})
 
     global __jupyter_config
     __jupyter_config = await __jupyter_config_future
@@ -23,9 +23,10 @@ async def _get_jupyter_config():
 
 # This task will be complete once __jupyter_config is populated.
 __jupyter_config_task = asyncio.create_task(_get_jupyter_config())
-    
+
+
 async def _request_jupyter_config(timeout=1):
-    """ Attempt to complete task to retrieve jupyter config.
+    """Attempt to complete task to retrieve jupyter config.
 
     This coroutine will timeout after arg timeout seconds.
     This coroutine can be awaited multiple times in case time is too short.
@@ -41,7 +42,13 @@ async def _request_jupyter_config(timeout=1):
     try:
         await asyncio.wait_for(asyncio.shield(__jupyter_config_task), timeout=timeout)
     except asyncio.TimeoutError:
-        raise RuntimeException('Jupyter config not ready, re-run "await JupyterDash.infer_jupyter_proxy_config()"')
+        raise RuntimeError(
+            (
+                'Jupyter config not ready, re-run "await '
+                'JupyterDash.infer_jupyter_proxy_config()"'
+            )
+        )
+
 
 def get_jupyter_config():
     global __jupyter_config
@@ -50,10 +57,18 @@ def get_jupyter_config():
         return {}
 
     if not __jupyter_config_future.done():
-        raise RuntimeException('Jupyter config not ready, re-run "await JupyterDash.infer_jupyter_proxy_config()"')
+        raise RuntimeError(
+            (
+                'Jupyter config not ready, re-run "await '
+                'JupyterDash.infer_jupyter_proxy_config()"'
+            )
+        )
 
     return __jupyter_config
 
 
 def dash_send(message):
+    # Re-open the comm in case the frontend connection was lost (e.g. tab closed or
+    # refreshed).
+    __dash_comm.open()
     __dash_comm.send(message)
